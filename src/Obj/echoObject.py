5d111104440d13cab2b0089f0db8f5e3b15b20e9
BrianTurza <43517172+BrianTurza@users.noreply.github.com>
2018-11-17 00:24:47 +0100

    Update echoObject.py

--- src/Obj/echoObject.py
+++ src/Obj/echoObject.py
@@ -1,1 +1,14 @@
+class echoObject(object):
 
+    def __init__(self):
+        self.exec_str = ""
+    
+    def transpile_print(self,value):
+        self.exec_str += "print("+value+")" + "\n"
+        
+        return self.exec_str
+    
+    def transpile_input(self,value):
+        self.exec_str += "input("+value+")" + "\n"
+        
+        return self.exec_str
