04068f9870f985bbeba1eb0850025d69eb07995a
BrianTurza <43517172+BrianTurza@users.noreply.github.com>
2018-11-25 16:22:18 +0100

    Update lexer.py

--- src/v02/lexer.py
+++ src/v02/lexer.py
@@ -5,6 +5,42 @@ class Lexer(object):
     def __init__(self, source_code):
         self.source_code = source_code
 
+    
+    def getMatcher(self, matcher, current_index, source_code):
+
+        if source_code[current_index].count('"') == 2:
+
+            word = source_code[current_index].partition('"')[-1].partition('"'[0])
+
+            if word[2] != '': return [ '"' + word[0] + '"', '', word[2] ]
+
+            else:  return [ '"' + word[0] + '"', '', '' ]
+        
+        else:
+
+            source_code = source_code[current_index:len(source_code)]
+
+            word = ""
+
+            iter_count = 0
+
+            for item in source_code:
+
+                iter_count += 1
+
+                word += item + " "
+
+                if matcher in item and iter_count != 1: 
+
+                    return [
+                        '"' + word.partition('"')[-1].partition('"'[0])[0] + '"',
+                        word.partition('"')[-1].partition('"'[0])[2],             
+                        iter_count - 1                                            
+                    ]
+
+                    break
+
+
     def token(self):
         tokens = []
         source_code = self.source_code.split()
@@ -17,14 +53,18 @@ class Lexer(object):
 
             if "\n" in word:
                 pass
+            
+            elif word[1:] == "#" and word == "include":tokens.append(["INCLUDE",word])
 
-            if word =="var":tokens.append(["VAR",word])
+            elif word =="var":tokens.append(["VAR",word])
             
             elif word == "Type":tokens.append(["TP",word])
             
             elif word == "echo":tokens.append(["ECHO",word])
 
             elif word == "input":tokens.append(["INPUT",word])
+            
+            elif word == "system":tokens.append(["SYSTEM",word])
             
             elif word[4:] == "sqrt(":tokens.append(["ROOT",word])
             
@@ -50,12 +90,6 @@ class Lexer(object):
                 else:
                     tokens.append(["INT",word])
             
-            elif word in '""':
-                if word[len(word) - 1] == ';':
-                    tokens.append(["STR",word[:-1]])
-                else:
-                    tokens.append(["STR",word])
-            
             elif word in "+-*/%=":
                 tokens.append(["OP",word])
             
@@ -64,6 +98,22 @@ class Lexer(object):
             
             elif word[2:] == "/*" or word[:-2] == "*/":
                 tokens.append(["COMMENT",word])
+            
+            elif ('"') in word: 
+
+                matcherReturn = self.getMatcher('"', source_index, source_code)
+
+                if matcherReturn[1] == '': tokens.append(["STRING", matcherReturn[0]])
+
+                else:
+
+                    tokens.append(["STR", matcherReturn[0] ])
+                    
+                    if ';' in matcherReturn[1]: tokens.append(["SEMIC", ";"])
+
+                    source_index += matcherReturn[2]
+                    
+                    pass
 
             if word[len(word) -1]==";":
                 tokens.append(["SEMIC",';'])
